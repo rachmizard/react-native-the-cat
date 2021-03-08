1d95ca2ec9f63f0471fef94720125f732935fbcf
Rachmizard <rachmizard11072000@gmail.com>
2021-03-08 22:52:18 +0700

    add error config interceptor

--- src/config/axios/index.js
+++ src/config/axios/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import {ToastAndroid} from 'react-native';
 
 const Axios = axios.create({
   baseURL: 'https://api.thecatapi.com/v1',
@@ -9,5 +10,20 @@ const Axios = axios.create({
 
 Axios.defaults.headers.common['x-api-key'] =
   'd82040a9-5732-44d3-85aa-a9c6280a145f';
+
+Axios.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (error) => {
+    Promise.reject(error).then((error) =>
+      ToastAndroid.showWithGravity(
+        error,
+        ToastAndroid.SHORT,
+        ToastAndroid.CENTER,
+      ),
+    );
+  },
+);
 
 export default Axios;
